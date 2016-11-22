987008466210702b4d1e80a0e3ea5c1d54d1634a
Andre Tättar <b02166@ut.ee>
2016-11-22 15:13:30 +0200

    Common and server error handling

--- server/server.py
+++ server/server.py
@@ -1,31 +1,53 @@
 import socket
 from Protocol import ServerProtocol
+from common import DEFAULT_HOST, DEFAULT_PORT, tcp_recieve, tcp_send
+
 
 class Server:
     def __init__(self):
-        self.socket = self.socket_init("localhost", 49995)
+        self.socket = self.socket_init(DEFAULT_HOST, DEFAULT_PORT)
         self.editor = ServerProtocol()
         self.run()
 
     def socket_init(self, server_ip, port):
         """ Socket Initialization """
         backlog = 5
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((server_ip, port))
-        s.listen(backlog)
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind((server_ip, port))
+            s.listen(backlog)
+        except socket.error as e:
+            print("Socket error: %s" % str(e))
+            exit(1)
         return s
 
     def run(self):
+        client = None
         while 1:
-            client, accept = self.socket.accept()
-            print("Incoming connection")
-            print(client)
-            print(accept)
-            while 1:
-                msg, whaa = client.recvfrom(1024)
-                response = self.editor.handleEvent(msg)
-                client.send(response)
-                print(msg)
+            try:
+                client, source = self.socket.accept()
+                print("Incoming connection: %s - %s" % (client, source))
+                try:
+                    msg = tcp_recieve(client)
+                    response = self.editor.handleEvent(msg)
+                    tcp_send(client, response)
+                except socket.error as e:
+                    print("Some error: %s" % (str(e)))
+
+            except KeyboardInterrupt:
+                print('Ctrl+C - terminating server')
+                break
+
+        if client is not None:
+            try:
+                client.close()
+            except socket.error:
+                print('Client disconnected')
+
+        self.socket.close()
+        print('Server socket closed.')
+
+
 
 if __name__ == "__main__":
     srvr = Server()
