7a55129a1d18082349440620e7f8b727ea19c6ee
tarstarkvara <tarssander1@gmail.com>
2016-11-22 11:50:41 +0000

    Protocol types added, dummy structure for handling.

--- server/Protocol.py
+++ server/Protocol.py
@@ -4,5 +4,53 @@ class ServerProtocol:
 
     def handleEvent(self, eventString):
         print(eventString)
-        msg = eventString.split("*")[0]
+        ##TODO: kuidagi peab handlima ka lockimise protokolli, tõenäoliselt teiste protokollide sees
+        ##TODO: näiteks newline sümboli puhul vms
+        protocolType = eventString.split('*')[1]
+        if protocolType == 'insert':
+            self.insertProtocol()
+        elif protocolType == 'delete':
+            self.deleteProtocol()
+        elif protocolType == 'swap':
+            self.swapProtocol()
+        elif protocolType == 'undo':
+            self.undoProtocol()
+        elif protocolType == 'redo':
+            self.redoProtocol()
+        elif protocolType == 'open':
+            self.openProtocol()
+        elif protocolType == 'new':
+            self.newProtocol()
+        elif protocolType == 'authent':
+            self.authentProtocol()
+        msg = eventString.split("*")[1]
         return msg
+    def insertProtocol(self):
+        ##olemas sümbol, asukoht reas, reanumber
+        return None
+
+    def deleteProtocol(self):
+        ##olemas sümbol, asukoht reas, reanumber
+        return None
+
+    def swapProtocol(self):
+        ##olemas sümbol, asukoht reas, reanumber
+        return None
+
+    def undoProtocol(self):
+        return None
+
+    def redoProtocol(self):
+        return None
+
+    def lockProtocol(self):
+        return None
+
+    def openProtocol(self):
+        return None
+
+    def newProtocol(self):
+        return None
+
+    def authentProtocol(self):
+        return None
