36b4f653501cc97711fd875fd7715082402df2e2
Andre Tättar <b02166@ut.ee>
2016-11-23 14:37:31 +0200

    Passwordhandlers, filehandlers, tests

--- server/file_io.py
+++ server/file_io.py
@@ -1,6 +1,8 @@
 from os import listdir
 from os.path import exists, join, abspath, dirname
 
+from PasswordHandler import PasswordHandler
+
 
 class FileHandler:
     dump_dir = 'dump'
@@ -8,23 +10,30 @@ class FileHandler:
     def __init__(self):
         self.fname = ""
         self.cur_dir = dirname(abspath(__file__))
+        self.pwdb = PasswordHandler()
 
-    def new_file(self, fstring):
+    def new_file(self, fstring, passw = ""):
         fs = join(self.cur_dir, self.dump_dir, fstring)
         if exists(fs):
             return "File with such name already exists", None
         else:
             self.fname = fs
+            self.pwdb.new_pw(fs, passw)
             return "OK", []
 
     def list_files(self):
-        return str(listdir(join(self.cur_dir, self.dump_dir)))
+        return listdir(join(self.cur_dir, self.dump_dir))
 
-    def open_file(self, fstring):
+    def open_file(self, fstring, passw = ""):
         fs = join(self.cur_dir, self.dump_dir, fstring)
-        if exists(fs):
-            self.fname = fs
+        if fs in self.pwdb.db:
+            if self.pwdb.pw_correct(fs, passw):
+                self.fname = fs
+            else:
+                return "Password Incorrect", None
         else:
+            print(fstring)
+            print(self.pwdb.db)
             return "File with such name does not exist", None
         txt = []
         try:
@@ -36,6 +45,7 @@ class FileHandler:
         return "OK", txt
 
     def save(self, txt):
+        self.pwdb.save()
         try:
             with open(self.fname, "w") as f:
                 for line in txt:
