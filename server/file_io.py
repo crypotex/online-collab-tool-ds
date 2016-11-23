4f8c8baf15182131ddc3da2cb3f4f934b9bcece9
Andre Tättar <b02166@ut.ee>
2016-11-23 11:58:55 +0200

    Minor consistency fix with paths

--- server/file_io.py
+++ server/file_io.py
@@ -9,7 +9,7 @@ class FileHandler:
         self.fname = ""
 
     def new_file(self, fstring):
-        fs = "%s/%s" % (self.dump_dir, fstring)
+        fs = join(self.dump_dir, fstring)
         if exists(fs):
             return "File with such name already exists", None
         else:
